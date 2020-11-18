0993b8d2fa70061eeb51ed640af151f0231aeb35
MaksimRilov <mksm.rilov@gmail.com>
2020-11-18 13:21:24 +0300

    server-dev Добавлены миграции таблиц: tasks, statuses

--- server/db/seeders/20201111084111-root-user.js
+++ server/db/seeders/20201111084111-root-user.js
@@ -5,15 +5,6 @@ const salt = bcrypt.genSaltSync(10);
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
     await queryInterface.bulkInsert(
       'users',
       [
@@ -34,12 +25,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     await queryInterface.bulkDelete('users', null);
   },
 };
