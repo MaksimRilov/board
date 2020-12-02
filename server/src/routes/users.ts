f2a55d335ea46799e5b4d9823e3d34b138f026d8
MaksimRilov <mksm.rilov@gmail.com>
2020-12-02 13:52:32 +0300

    server-dev Изменены end-points

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -4,16 +4,16 @@ import { UserController } from '../controllers';
 
 const userRouter = express.Router();
 
-userRouter.get('', passport.authenticate('jwt', { session: false }), UserController.UserGet.me);
+userRouter.get('/me', passport.authenticate('jwt', { session: false }), UserController.UserGet.me);
 
 userRouter.get(
-  '/all',
+  '',
   passport.authenticate('jwt', { session: false }),
   UserController.UserGet.allUsers
 );
 
 userRouter.post(
-  '',
+  '/register',
   passport.authenticate('jwt', { session: false }),
   UserController.UserPost.register
 );
